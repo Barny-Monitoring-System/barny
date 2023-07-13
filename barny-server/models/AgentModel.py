8d4d0fd7466962d4a419de6748c49b704a594cf4
Maksymov Artem <maximov.artyom007@gmail.com>
2023-07-13 17:04:32 +0300

    Add basic agent route

---
+++ barny-server/models/AgentModel.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import Dict, Any
+
+class AgentModel(BaseModel):
+    name: str | None = None
+    ip: str | None = None
+    domain: str | None = None
+    
+    def dict(self, *args, **kwargs) -> Dict[str, Any]:
+        kwargs.pop('exclude_none', None)
+        return super().dict(*args, exclude_none=True, **kwargs)
