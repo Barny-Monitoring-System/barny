a5e83523da9d6d26ecb84549eb551c23011dd448
Maksymov Artem <maximov.artyom007@gmail.com>
2023-07-14 00:35:04 +0300

    Add agent table section

---
+++ barny-server-ui/src/components/Table.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let content;
+
+    console.log("The content is");
+    console.log(content);
+    
+</script>
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Ip</th>
+        <th>Domain</th>
+    </tr>
+    {#each content.data as agent}
+    <tr>
+        <th>{agent.name}</th>
+        <th>{agent.ip}</th>
+        <th>{agent.domain}</th>
+    </tr>
+    {/each}
+</table>
+
+<style>
+    
+</style>
--- barny-server-ui/src/routes/agents/+page.svelte
+++ barny-server-ui/src/routes/agents/+page.svelte
@@ -1,7 +1,16 @@
 <script>
     import Button    from "../../components/buttons/Button.svelte";
     import SearchBar from "../../components/SearchBar.svelte";
+    import Table from "../../components/Table.svelte";
+    import axios from 'axios';
+    
+    let content = axios.get("http://127.0.0.1:8000/agents")
+                       .catch(err => `Something bad happened: ${err}`);
 </script>
 
 
 <SearchBar></SearchBar>
+
+{#await content then content}
+  <Table {content} ></Table>
+{/await}
